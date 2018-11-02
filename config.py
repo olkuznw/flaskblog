22e174862b8706c89b2d8e3bbeeaebda8ffbcf6d
olkuznw <olkuznw@gmail.com>
2018-11-02 23:47:24 +0300

    blog work

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.evnviron.get('SECRET_KEY') or 'QhIUeulzGyyV5uJ8eurm'
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'QhIUeulzGyyV5uJ8eurm'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
 
@@ -11,14 +11,14 @@ def init_app(app):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + ':'.join([os.environ.get('BD_DEV_USER'), os.environ.get('BD_DEV_PASS')]) + ':password@localhost/' + os.environ.get('BD_DEV_NAME')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + ':'.join([os.environ.get('DB_DEV_USER'), os.environ.get('DB_DEV_PASS')]) + ':password@localhost/' + os.environ.get('DB_DEV_NAME')
 
 class TestConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + ':'.join([os.environ.get('BD_TEST_USER'), os.environ.get('BD_TEST_PASS')]) + ':password@localhost/' + os.environ.get('BD_TEST_NAME')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + ':'.join([os.environ.get('DB_TEST_USER'), os.environ.get('DB_TEST_PASS')]) + ':password@localhost/' + os.environ.get('DB_TEST_NAME')
 
 class LiveConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + ':'.join([os.environ.get('BD_LIVE_USER'), os.environ.get('BD_LIVE_PASS')]) + ':password@localhost/' + os.environ.get('BD_LIVE_NAME')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + ':'.join([os.environ.get('DB_LIVE_USER'), os.environ.get('DB_LIVE_PASS')]) + ':password@localhost/' + os.environ.get('DB_LIVE_NAME')
 
 config = {
     'dev': DevelopmentConfig,
